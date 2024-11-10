1c4dd459dd273900e10ee5fb08e4a56343a2fad8
Narender <narenderyadagiri39@gmail.com>
2024-11-10 22:46:03 +0530

    simple.java

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
 class simple{
     public static void main(String args[]){
-    System.out.println("hello java world");
+    System.out.println("Narender 528\nLokesh 524\nSneha 552 \nSrinija 557");
 }
 }
